bef535c461c03dca5b66574e3658424aecb890a1
andrewrodgers90 <132149730+andrewrodgers90@users.noreply.github.com>
2024-07-06 00:35:50 +0700

    Time: 19 ms (30.62%) | Memory: 6.2 MB (39.09%) - LeetSync

--- 326-power-of-three/power-of-three.c
+++ 326-power-of-three/power-of-three.c
@@ -1,18 +1,10 @@
 bool isPowerOfThree(int n)
 {
-    long long int i;
-
     if (n == 1)
         return (true);
     if (n < 1)
         return (false);
-    i = 1;
-    while (1)
-    {
-        i *= 3;
-        if (i == n)
-            return (true);
-        if (i > n)
-            return (false);
-    }
+    while (n % 3 == 0)
+        n /= 3;
+    return (n == 1);
 }
