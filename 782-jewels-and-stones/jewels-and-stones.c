ab140b208bd121b1da2efacbfd293c2a2b8b31de
andrewrodgers90 <132149730+andrewrodgers90@users.noreply.github.com>
2024-07-09 23:52:28 +0700

    Time: 4 ms (6.51%) | Memory: 5.6 MB (12.74%) - LeetSync

---
+++ 782-jewels-and-stones/jewels-and-stones.c
@@ -0,0 +1,15 @@
+int numJewelsInStones(char* jewels, char* stones)
+{
+    int     count;
+
+    count = 0;
+    for (int i = 0; i < strlen(jewels); i++)
+    {
+        for (int j = 0; j < strlen(stones); j++)
+        {
+            if (jewels[i] == stones[j])
+                count++;
+        }
+    }
+    return (count);
+}
