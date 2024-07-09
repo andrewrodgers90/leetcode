8080355390dbd00ad79e9fee9248d09cd2f20a15
andrewrodgers90 <132149730+andrewrodgers90@users.noreply.github.com>
2024-07-09 23:36:33 +0700

    Time: 4 ms (18.36%) | Memory: 5.6 MB (57.60%) - LeetSync

--- 389-find-the-difference/find-the-difference.c
+++ 389-find-the-difference/find-the-difference.c
@@ -1,31 +1,10 @@
 char findTheDifference(char* s, char* t)
 {
-    int     len1 = strlen(s);
-    int     len2 = strlen(t);
-    int     total_len = len1 + len2;
-    int     i = 0;
-    int     j = 0;
-    char    *combined;
     char    new = 0;
 
-    combined = (char *)malloc(sizeof(char) * (len1 + len2 + 1));
-    if (NULL == combined)
-        return (NULL);
-    
-    while (i < strlen(s))
-    {
-        combined[i] = s[i];
-        i++;
-    }
-    while (j < strlen(t))
-    {
-        combined[i + j] = t[j];
-        j++;
-    }
-    combined[i + j] = '\0';
-    for (int k = 0; k < strlen(combined); k++)
-    {
-        new ^= combined[k];
-    }
+    for (int i = 0; i < strlen(s); i++)
+        new ^= s[i];
+    for (int j = 0; j < strlen(t); j++)
+        new ^= t[j];
     return (new);
 }
