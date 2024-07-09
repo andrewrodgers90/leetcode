1a8820a153d06830510dfff484e0c289fbb81cb7
andrewrodgers90 <132149730+andrewrodgers90@users.noreply.github.com>
2024-07-09 23:37:12 +0700

    Time: 3 ms (54.76%) | Memory: 5.7 MB (45.74%) - LeetSync

--- 389-find-the-difference/find-the-difference.c
+++ 389-find-the-difference/find-the-difference.c
@@ -1,10 +1,12 @@
 char findTheDifference(char* s, char* t)
 {
     char    new = 0;
+    int     len1 = strlen(s);
+    int     len2 = strlen(t);
 
-    for (int i = 0; i < strlen(s); i++)
+    for (int i = 0; i < len1; i++)
         new ^= s[i];
-    for (int j = 0; j < strlen(t); j++)
+    for (int j = 0; j < len2; j++)
         new ^= t[j];
     return (new);
 }
