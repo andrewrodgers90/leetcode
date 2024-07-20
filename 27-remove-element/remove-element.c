2ff12f8710c21285b029a9790cc05dea2043bca8
andrewrodgers90 <132149730+andrewrodgers90@users.noreply.github.com>
2024-07-20 21:17:37 +0700

    Time: 5 ms (11.69%) | Memory: 9.5 MB (6.84%) - LeetSync

---
+++ 27-remove-element/remove-element.c
@@ -0,0 +1,17 @@
+int removeElement(int* nums, int numsSize, int val)
+{
+    int     i;
+    int     k;
+
+    i = 0;
+    k = 0;
+    for (int i = 0; i < numsSize; i++)
+    {
+        if (nums[i] != val)
+        {
+            nums[k] = nums[i];
+            k++;
+        }
+    }
+    return (k);
+}
